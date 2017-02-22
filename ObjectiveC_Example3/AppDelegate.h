724a5067ad4ff95d1d03d8f76e8fad1812e9a904
masangbeom <sangbum@favorie.co>
2017-02-22 15:33:08 +0900

    Initial Commit

---
+++ ObjectiveC_Example3/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ObjectiveC_Example3
+//
+//  Created by sangbeom ma on 2017. 2. 22..
+//  Copyright © 2017년 favorie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ObjectiveC_Example3/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ObjectiveC_Example3
+//
+//  Created by sangbeom ma on 2017. 2. 22..
+//  Copyright © 2017년 favorie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
